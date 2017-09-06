20e675b289a6956fceb3e3b83ca046379b5eecf0
spickering@outlook.com <NJoyce111174$>
2017-09-06 13:18:22 -0700

    fixed pathfor tmqrscripts

--- tmqrscripts/run_all_history_contract_update.py
+++ tmqrscripts/run_all_history_contract_update.py
@@ -1,6 +1,9 @@
 '''
 Futures data updates script, all futures contracts
 '''
+import sys
+sys.path.append('..')
+#sys.path.append('/tmqr_framework_2/')
 
 from tmqrscripts.load_futures_from_v1 import run_all_futures
 
