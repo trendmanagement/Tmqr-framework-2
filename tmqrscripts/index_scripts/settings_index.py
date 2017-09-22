04db89f44e432b4c0b7e7891e802ef92fbed01d6
spickering@outlook.com <NJoyce111174$>
2017-09-22 12:21:03 -0700

    added new index classes to setting_index script

--- tmqrscripts/index_scripts/settings_index.py
+++ tmqrscripts/index_scripts/settings_index.py
@@ -1,5 +1,5 @@
 from tmqrindex.deployed.exo_long_enhance_dt import EXOLongEnhance_DT
-from tmqrindex.deployed.exo_long_enhance_dt_callspread import EXOLongEnhance_DT_CallSpread
+from tmqrindex.deployed.exo_long_enhance_dt_putspread import EXOLongEnhance_DT_PutSpread
 from tmqrindex.deployed.exo_short_enhance_dt_2 import EXOShortEnhance_DT_2
 from tmqrindex.deployed.exo_short_enhance_dt_2_callspread import EXOShortEnhance_DT_2_CallSpread
 
@@ -9,7 +9,7 @@ INDEX_LIST = [
         'class': EXOLongEnhance_DT
     },
     {
-        'class': EXOLongEnhance_DT_CallSpread
+        'class': EXOLongEnhance_DT_PutSpread
     },
     {
         'class': EXOShortEnhance_DT_2
