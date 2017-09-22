2c7cd83cdc7d6a308574aa6aae0ab9637e178a32
spickering@outlook.com <NJoyce111174$>
2017-09-22 13:52:19 -0700

    TESTING 1

--- tmqrscripts/run_indexes.py
+++ tmqrscripts/run_indexes.py
@@ -4,7 +4,7 @@ from tmqr.settings import *
 from tmqrfeed.manager import DataManager
 from tmqrindex import IndexBase
 
-#from tmqrstrategy.strategy_base import StrategyBase
+from tmqrstrategy.strategy_base import StrategyBase
 
 from tmqr.logs import log
 from datetime import datetime
@@ -57,9 +57,9 @@ class IndexGenerationScript:
         #             t.start()
 
         for exo in INDEX_LIST:
-          #self.run_through_each_index_threads('US.ES', exo)
-          t = threading.Thread(target=self.run_through_each_index_threads, args=('US.ES', exo))
-          t.start()
+           # self.run_through_each_index_threads('US.ES', exo)
+           t = threading.Thread(target=self.run_through_each_index_threads, args=('US.ES', exo))
+           t.start()
 
         # for instrument in self.asset_info_collection.find({}):
         #     if not 'DEFAULT' in instrument['instrument']:
@@ -217,9 +217,9 @@ class IndexGenerationScript:
         try:
             dm2 = DataManager()
             #print(alpha_name)
-            # saved_alpha = StrategyBase.load(dm2, alpha_name)
-            # saved_alpha.run()
-            # saved_alpha.save()
+            saved_alpha = StrategyBase.load(dm2, alpha_name)
+            saved_alpha.run()
+            saved_alpha.save()
 
             print(alpha_name)
 
