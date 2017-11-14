fb30b5f1fdfbedb57c50689865f5e4c3a2d24f62
spickering@outlook.com <NJoyce111174$>
2017-11-14 09:01:02 -0800

    added error catch to exo and alpha run with slack message on exo or alpha failure

--- tmqrscripts/run_indexes/run_indexes.py
+++ tmqrscripts/run_indexes/run_indexes.py
@@ -6,8 +6,8 @@ from tradingcore.execution_manager import ExecutionManager
 from exobuilder.data.assetindex_mongo import AssetIndexMongo
 from exobuilder.data.exostorage import EXOStorage
 
-# from tradingcore.signalapp import SignalApp, APPCLASS_EXO, APPCLASS_ALPHA
-# from tradingcore.messages import *
+from tradingcore.signalapp import SignalApp, APPCLASS_EXO, APPCLASS_ALPHA
+from tradingcore.messages import *
 
 try:
     '''C - compiled code, will not run on local machine'''
@@ -26,7 +26,7 @@ from tmqr.logs import log
 from datetime import datetime, time
 import pytz
 from tmqr.errors import DataEngineNotFoundError
-import threading
+# import threading
 
 log.disabled = True
 
@@ -90,7 +90,7 @@ class IndexGenerationScript:
 
         self.campaign_exo_list = self.get_campaign_exo_list(self.campaign_alpha_list)
 
-        # self.signalapp_exo = SignalApp('V2 calcs', APPCLASS_EXO, RABBIT_HOST, RABBIT_USER, RABBIT_PASSW)
+        self.signalapp_exo = SignalApp('V2 calcs', APPCLASS_EXO, RABBIT_HOST, RABBIT_USER, RABBIT_PASSW)
         # self.signalapp_alpha = SignalApp('V2 calcs', APPCLASS_ALPHA, RABBIT_HOST, RABBIT_USER, RABBIT_PASSW)
 
     # def run(self):
@@ -285,10 +285,8 @@ class IndexGenerationScript:
 
         # if not creating_index:
         try:
-            # self.mongo_db_v2['index_data'].update_one({'name': index_hedge_name},
-            #                                           {'$set': {'context.index_update_time': update_time}})
-
-
+            self.mongo_db_v2['index_data'].update_one({'name': index_hedge_name},
+                                                      {'$set': {'context.index_update_time': update_time}})
 
             index.run()
             index.save()
@@ -297,9 +295,11 @@ class IndexGenerationScript:
             self.mongo_db_v2['index_data'].update_one({'name': index_hedge_name},
                                                       {'$set': {'context.index_update_time': update_time}})
 
-        except:
-            # pass
+        except Exception as e:
             log.warn("Failed Index {0}".format(index_hedge_name) )
+            log.exception(e)
+            self.signalapp_exo.send(
+                MsgStatus('V2_Index', 'V2 Index Error {0}'.format(index_hedge_name), notify=True))
 
         # self.signalapp_exo.send(MsgStatus('V2_Index', 'V2 Index finished {0}'.format(index_hedge_name), notify=True))
         #pass
@@ -396,13 +396,19 @@ class IndexGenerationScript:
             saved_alpha = StrategyBase.load(dm2, alpha_name)
             saved_alpha.run()
             saved_alpha.save()
+
+            self.mongo_db_v2['alpha_data'].update_one({'name': alpha_name},
+                                                      {'$set': {'context.alpha_end_update_time': update_time}})
         except:
             log.warn("Failed Alpha {0}".format(alpha_name))
+            log.exception(e)
+            self.signalapp_exo.send(
+                MsgStatus('V2_Alpha', 'V2 Alpha Error {0}'.format(alpha_name), notify=True))
 
         #print('running finished ' + alpha_name)
 
-        self.mongo_db_v2['alpha_data'].update_one({'name': alpha_name},
-                                                  {'$set': {'context.alpha_end_update_time': update_time}})
+        # self.mongo_db_v2['alpha_data'].update_one({'name': alpha_name},
+        #                                           {'$set': {'context.alpha_end_update_time': update_time}})
 
         # self.signalapp_alpha.send(MsgStatus('V2_Alpha', 'V2 Alpha finished {0}'.format(alpha_name), notify=True))
 
