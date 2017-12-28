2d9706432cb711cc1f34e14112514775fce0dd9b
AlexVeden <i@alexveden.com>
2017-12-28 10:26:27 +0400

    - Added IQConnect.exe crash checks

--- tmqrscripts/data/run_iqfeed_updates.py
+++ tmqrscripts/data/run_iqfeed_updates.py
@@ -475,6 +475,12 @@ if __name__ == "__main__":
                                     bars_data = hist_conn.request_bars_for_days(ticker=iq_ticker, interval_len=60, interval_type='s',
                                                                                 days=10, max_bars=10,
                                                                                 ascend=True, timeout=10)
+
+                                if len(bars_data) == 0:
+                                    # Unexpected behavior which means that IQFeed freezes, usualy it throws NO DATA exception, when there is no data
+                                    log.error("Suspected IQConnect.exe crash, it always must return bars or raise NoData exception")
+                                    sys.exit(-3)
+
                                 # Fix: Historical data comes without ticker, do monkey patch
                                 bar_listener.process_history_bar(bars_data,
                                                                  force_db_write=True,
