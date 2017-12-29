9eb46d25a538dae119c915fce6ee049ee2a067aa
AlexVeden <i@alexveden.com>
2017-12-29 08:17:03 +0400

    - IQFeed shell script fix

--- tmqrscripts/data/run_iqfeed_script.sh
+++ tmqrscripts/data/run_iqfeed_script.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
-killall iqconnect.exe && killall -9 iqconnect.exe && /home/tmqr_framework2/anaconda3/bin/python /home/tmqr_framework2/tmqr_framework2/tmqrscripts/data/run_iqfeed_updates.py --headless --live_n_futures=6
+
+# Try gracefully kill iqconnect.exe first
+killall iqconnect.exe
+
+# Otherwise try to force kill
+killall -9 iqconnect.exe
+
+# Run the script
+/home/tmqr_framework2/anaconda3/bin/python /home/tmqr_framework2/tmqr_framework2/tmqrscripts/data/run_iqfeed_updates.py --headless --live_n_futures=6
